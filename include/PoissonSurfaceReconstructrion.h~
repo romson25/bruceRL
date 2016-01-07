3baeb5ed064d76a48ac5cf5c390f30f2a91e1314
adam <adamswiacik92@gmail.com>
2016-01-07 16:31:42 +0100

    first commit

---
+++ include/PoissonSurfaceReconstructrion.h~
@@ -0,0 +1,46 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/Poisson_reconstruction_function.h>
+#include <CGAL/compute_average_spacing.h>
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Point_with_normal_3.h>
+#include <CGAL/Implicit_surface_3.h>
+#include <CGAL/property_map.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/trace.h>
+
+#include <CGAL/IO/output_surface_facets_to_polyhedron.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/IO/read_xyz_points.h>
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::Point_with_normal_3<Kernel> Point_with_normal;
+typedef Kernel::FT FT;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Sphere_3 Sphere;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef std::vector<Point_with_normal> PointList;
+typedef CGAL::Surface_mesh_default_triangulation_3 STr;
+typedef CGAL::Surface_mesh_complex_2_in_triangulation_3<STr> C2t3;
+typedef CGAL::Poisson_reconstruction_function<Kernel> Poisson_reconstruction_function;
+typedef CGAL::Implicit_surface_3<Kernel, Poisson_reconstruction_function> Surface_3;
+
+class PoissonSurfaceReconstructrion
+{
+public:
+PoissonSurfaceReconstructrion(std::string fileName);
+
+std::string reconstruct();
+
+private:
+	void setFilesName(std::string fileName);
+
+	std::string outputFileName;
+	std::string inputFileName;
+
+};
---
+++ src/SetPointProcessing.cpp
@@ -0,0 +1,130 @@
+#include "SetPointProcessing.h"
+
+SetPointProcessing::SetPointProcessing(std::string fileName)
+{
+setFilesName(fileName);
+}
+std::string SetPointProcessing::compute()
+{
+	read();
+	removeOutlier(removeOutlierPercentage, removeOutlierNeighbor);
+	wlopSimplify(wlopSimplifyPercentage, wlopSimplifyNeighbor);
+	smooth(smoothNeighbor);
+    	write();
+
+	readPca();
+	pcaEstimateNormals(pcaEstimateNormalsNeighbor);
+	writePca();
+
+    return outputFileName;
+}
+
+void SetPointProcessing::read()
+{
+    std::ifstream stream(inputFileName.c_str());
+
+    if( CGAL::read_xyz_points(stream, std::back_inserter(inPoints)) && !inPoints.empty() )
+	std::cout<<"SetPointProcessing::read\tvertices\t"	<<inPoints.size()	<<std::endl;
+else
+	std::cout << "SetPointProcessing:: read\tERROR:\t" 	<<inputFileName	<<std::endl;
+
+    stream.close();
+}
+void SetPointProcessing::readPca()
+{
+    std::ifstream stream(temporaryFileName.c_str());
+
+    if(CGAL::read_xyz_points(stream, std::back_inserter(points), CGAL::First_of_pair_property_map<PointVectorPair>()) && !points.empty() )
+	std::cout<<"SetPointProcessing::readPca\tvertices\t"		<<points.size()		<<std::endl;
+else
+	std::cerr << "SetPointProcessing:: readPca\tERROR:\t"	<<temporaryFileName	<<std::endl;
+
+stream.close();
+}
+void SetPointProcessing::write()
+{
+    std::ofstream stream(temporaryFileName.c_str());
+
+    if(CGAL::write_xyz_points(stream, outPoints.begin(), outPoints.end()))
+	std::cout<<"SetPointProcessing::write\tvertices\t"	<<outPoints.size()		<<std::endl;
+else
+	std::cerr << "SetPointProcessing:: write\tERROR:\t"	<<temporaryFileName	<<std::endl;
+
+    stream.close();
+}
+std::string SetPointProcessing::writePca()
+{
+    std::ofstream stream(outputFileName.c_str());
+
+    if(CGAL::write_xyz_points_and_normals
+            (
+                stream,
+                points.begin(),
+                points.end(),
+                CGAL::First_of_pair_property_map<PointVectorPair>(),
+                CGAL::Second_of_pair_property_map<PointVectorPair>()
+            )
+        )
+	std::cout<<"SetPointProcessing::writePca\tvertices\t"		<<points.size()		<<std::endl;
+else
+	std::cerr << "SetPointProcessing:: writePca\tERROR:\t"	<<outputFileName	<<std::endl;
+
+    stream.close();
+
+return outputFileName;
+}
+void SetPointProcessing::setFilesName(std::string fileName)
+{
+    inputFileName       = fileName;
+    outputFileName      = fileName;
+    temporaryFileName   = fileName;
+
+    const std::string set  = "_set";
+    const std::string temp = "_temp";
+
+    int pos = fileName.find_last_of(".");
+
+    outputFileName.insert(pos, set);
+    temporaryFileName.insert(pos, temp);
+}
+
+void SetPointProcessing::removeOutlier(const double removedPercentage, unsigned int neighborNumber)
+{
+    inPoints.erase(CGAL::remove_outliers
+                   (
+                       inPoints.begin(), inPoints.end(),
+                       CGAL::Identity_property_map<Point>(),
+                       neighborNumber, removedPercentage
+                    ),
+                   inPoints.end());
+
+    std::vector<Point>(inPoints).swap(inPoints);
+}
+void SetPointProcessing::wlopSimplify(const double retainPercentage, const double neighborRadius)
+{
+    CGAL::wlop_simplify_and_regularize_point_set<Concurrency_tag>
+            (
+                inPoints.begin(), inPoints.end(),
+                std::back_inserter(outPoints),
+                retainPercentage, neighborRadius
+            );
+}
+void SetPointProcessing::smooth(unsigned int neighborNumber)
+{
+    CGAL::jet_smooth_point_set<Concurrency_tag>(outPoints.begin(), outPoints.end(), neighborNumber);
+}
+void SetPointProcessing::pcaEstimateNormals(unsigned int neighborNumber)
+{
+    CGAL::pca_estimate_normals<Concurrency_tag>(points.begin(), points.end(),
+                               CGAL::First_of_pair_property_map<PointVectorPair>(),
+                               CGAL::Second_of_pair_property_map<PointVectorPair>(),
+                               neighborNumber);
+
+    std::list<PointVectorPair>::iterator unoriented_points_begin =
+      CGAL::mst_orient_normals(points.begin(), points.end(),
+                                 CGAL::First_of_pair_property_map<PointVectorPair>(),
+                                 CGAL::Second_of_pair_property_map<PointVectorPair>(),
+                                 neighborNumber);
+
+    points.erase(unoriented_points_begin, points.end());
+}
